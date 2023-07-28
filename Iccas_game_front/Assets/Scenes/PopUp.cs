b759ed9b82b41f36575f815e0a300d520bbf6596
Yunhui <kss7035@naver.com>
2023-07-28 16:05:34 +0900

    [feat]: 로그인 기능 구현 (#41)
    스크립트 파일 이상으로 소스코드 전체 주석처리

--- Iccas_game_front/Assets/Scenes/PopUp.cs
+++ Iccas_game_front/Assets/Scenes/PopUp.cs
@@ -1,14 +1,14 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PopUp : MonoBehaviour
 {
-    public GameObject popupPanel;
+   // public GameObject popupPanel;
 
     public void PopUp()
     {
-        popupPanel.SetActive(true);
+        //popupPanel.SetActive(true);
     }
     // Start is called before the first frame update
     void Start()
@@ -21,4 +21,4 @@ public class PopUp : MonoBehaviour
     {
         
     }
-}
+}*/
