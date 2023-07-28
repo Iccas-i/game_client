ce9ea503c0befaaaeb6282d9073cf9184069da59
shashum <shashum@daum.net>
2023-07-28 23:00:18 +0900

    [feat] 병합 후 작업 (#37)
    상점 배너 구현과 레이어 구분

--- Iccas_game_front/Assets/Scenes/ShopCloseBtn.cs
+++ Iccas_game_front/Assets/Scenes/ShopCloseBtn.cs
@@ -12,9 +12,7 @@ public class ShopCloseBtn : MonoBehaviour
         var seq = DOTween.Sequence();
 
         seq.Append(transform.DOScale(0.95f, 0.1f));
-        seq.Append(transform.DOScale(1.05f, 0.1f));
         seq.Append(transform.DOScale(1f, 0.1f));
-        seq.Append(transform.DOScaleY(3, 0.01f));
 
         seq.Play().OnComplete(() => {
             popupWindow.Hide();
