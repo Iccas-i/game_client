642188b1ca2c256ce4e62a0d3bc4e9e247778ce7
shashum <shashum@daum.net>
2023-07-28 20:07:54 +0900

    [feat]: 병합 후 작업 (#37)
    타이머 구현
    CloseButtonHandler 버그 임시 수정

--- Iccas_game_front/Assets/Scenes/CloseButtonHandler.cs
+++ Iccas_game_front/Assets/Scenes/CloseButtonHandler.cs
@@ -11,9 +11,10 @@ public class CloseButtonHandler : MonoBehaviour
     {
         var seq = DOTween.Sequence();
 
-        seq.Append(transform.DOScale(1f, 0.1f));
+        seq.Append(transform.DOScale(0.95f, 0.1f));
         seq.Append(transform.DOScale(1.05f, 0.1f));
         seq.Append(transform.DOScale(1f, 0.1f));
+        seq.Append(transform.DOScaleY(3, 0.01f));
 
         seq.Play().OnComplete(() => {
             popupWindow.Hide();
