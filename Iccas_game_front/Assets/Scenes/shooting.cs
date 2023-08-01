f90c8a3dbc19b7bd1317573f76b0653aeccad6f9
kimyenida <kimeun744@gmail.com>
2023-08-01 22:00:15 +0900

    슈팅게임구현

---
+++ Iccas_game_front/Assets/Scenes/shooting.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class shooting : MonoBehaviour
+{
+
+    public Transform firepoint;
+    public GameObject bulletPrefab;
+    public float bulletForce = 20f;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+          Shoot();
+        }
+    }
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firepoint.up * bulletForce, ForceMode2D.Impulse);
+
+    }
+}
