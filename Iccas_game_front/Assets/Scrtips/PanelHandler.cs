1846aa7539621eeadbf225f1d85649e18fb147e9
shashum <shashum@daum.net>
2023-08-02 15:35:20 +0900

    [design] 팀 게임 디자인 변경 (#74)
    Team_Indiv 패널 수정

--- Iccas_game_front/Assets/Scrtips/PanelHandler.cs
+++ Iccas_game_front/Assets/Scrtips/PanelHandler.cs
@@ -5,6 +5,11 @@ using DG.Tweening;
 
 public class PanelHandler : MonoBehaviour
 {
+    public float scaleindex;
+    public float scaleindexX;
+    public float scaleindexY;
+    public float hidescale;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,8 +28,9 @@ public class PanelHandler : MonoBehaviour
         var seq = DOTween.Sequence();
 
         // DOScale 의 첫 번째 파라미터는 목표 Scale 값, 두 번째는 시간입니다.
-        seq.Append(transform.DOScale(1.1f, 0.2f));
-        seq.Append(transform.DOScale(1f, 0.1f));
+        seq.Append(transform.DOScale(scaleindex, 0.1f));
+        seq.Append(transform.DOScaleX(scaleindexX, 0.1f));
+        seq.Append(transform.DOScaleY(scaleindexY, 0.1f));
 
         seq.Play();
     }
@@ -35,12 +41,13 @@ public class PanelHandler : MonoBehaviour
 
         transform.localScale = Vector3.one * 0.2f;
 
-        seq.Append(transform.DOScale(1.1f, 0.1f));
-        seq.Append(transform.DOScale(0.2f, 0.2f));
+        seq.Append(transform.DOScale(hidescale, 0.2f));
+        //seq.Append(transform.DOScaleX(hideX, 0.1f));
+        
 
         // OnComplete 는 seq 에 설정한 애니메이션의 플레이가 완료되면
         // { } 안에 있는 코드가 수행된다는 의미입니다.
-        // 여기서는 닫기 애니메이션이 완료된 후 객첼르 비활성화 합니다.
+        // 여기서는 닫기 애니메이션이 완료된 후 객체를 비활성화 합니다.
         seq.Play().OnComplete(() =>
         {
             gameObject.SetActive(false);
